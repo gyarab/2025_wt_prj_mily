59298a442d7099f0fac73117e1204d15fd4105f6
JachymMily <jachym.mily.s@gyarab.cz>
2026-03-20 08:41:39 +0100

    add models

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import Recept, Comment
+
+@admin.register(Recept)
+class ReceptAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- prj/app/models.py
+++ prj/app/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+# Create your models here.
+class Recept(models.Model):
+    title = models.CharField(max_length=255)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+
+class Comment(models.Model):
+    recept = models.ForeignKey(Recept, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=255)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
