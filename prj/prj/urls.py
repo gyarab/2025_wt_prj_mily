e735f4f0166b41be8b93a09d1e077cc9218cd3ab
JachymMily <jachym.mily.s@gyarab.cz>
2026-04-24 10:06:17 +0200

    Add API endpoints for recipes and update URL configuration

--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from app.api import api
 
 urlpatterns = [
+    path('api/', api.urls),
     path('admin/', admin.site.urls),
     path('', views.render_home, name="home"),
     path('about/', views.render_about, name="about"),
