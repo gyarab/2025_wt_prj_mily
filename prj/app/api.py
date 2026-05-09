84de10967858612b2656bcac285391927e4835ea
Jachym <jachym.mily.s@gyarab.cz>
2026-05-09 22:20:02 +0200

    Merge branch 'main' of https://github.com/gyarab/2025_wt_prj_mily

--- prj/app/api.py
+++ prj/app/api.py
@@ -1,8 +1,9 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
 from .models import Recept
-from ninja import Schema
+from  ninja import Schema, NinjaAPI
 
 api = NinjaAPI()
+
 
 class ReceptSchema(Schema):
     id: int
@@ -11,6 +12,47 @@ class ReceptSchema(Schema):
     kategorie: int
     autor: int
 
+
+class ReceptCreateSchema(Schema):
+    nazev: str
+    popis: str
+
+
+# GET seznam
 @api.get("/recepty", response=list[ReceptSchema])
 def get_recepty(request):
     return Recept.objects.all()
+
+
+# GET detail
+@api.get("/recepty/{recept_id}", response=ReceptSchema)
+def get_recept(request, recept_id: int):
+    return Recept.objects.get(id=recept_id)
+
+
+# POST vytvoření
+@api.post("/recepty")
+def create_recept(request, data: ReceptCreateSchema):
+
+    recept = Recept.objects.create(
+        nazev=data.nazev,
+        popis=data.popis,
+        kategorie_id=1,
+        autor_id=1
+    )
+
+    return {"id": recept.id}
+
+
+# PUT úprava
+@api.put("/recepty/{recept_id}")
+def update_recept(request, recept_id: int, data: ReceptCreateSchema):
+
+    recept = Recept.objects.get(id=recept_id)
+
+    recept.nazev = data.nazev
+    recept.popis = data.popis
+
+    recept.save()
+
+    return {"success": True}
