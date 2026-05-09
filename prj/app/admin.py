7bb9ddc7be782f2fd2b2f9dafaf90ff84fc28454
Jachym <jachym.mily.s@gyarab.cz>
2026-05-09 21:43:44 +0200

    Refactor models and admin registration for Kategorie, Recept, Ingredience, Postup, and Hodnoceni

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from .models import Recept, Comment
+from .models import Kategorie, Recept, Ingredience, Postup, Hodnoceni
+
+
+@admin.register(Kategorie)
+class KategorieAdmin(admin.ModelAdmin):
+    pass
+
 
 @admin.register(Recept)
 class ReceptAdmin(admin.ModelAdmin):
     pass
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+
+@admin.register(Ingredience)
+class IngredienceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Postup)
+class PostupAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Hodnoceni)
+class HodnoceniAdmin(admin.ModelAdmin):
     pass
