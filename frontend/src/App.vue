8c09c2ea45e12a6deab30cf3c39dcb1611bac90d
JachymMily <jachym.mily.s@gyarab.cz>
2026-06-03 20:27:00 +0200

    Add App.vue with header and router setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <RouterLink to="/">
+            <h1>FilmDB (vue)</h1>
+        </RouterLink>
+    </header>
+    <main>
+        <RouterView />
+    </main>
+
+</template>
